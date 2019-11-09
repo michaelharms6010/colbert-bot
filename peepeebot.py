e46cff4fc7def93228b0c012c7301766bb2952d6
Michael Harms <michael.harms6010@gmail.com>
2019-11-09 17:15:28 -0600

    expanded dictionary, formatted capitals

--- peepeebot.py
+++ peepeebot.py
@@ -1,8 +1,8 @@
 import random
 
 
-nouns = ["dictator", "Hitler", "fascist", "tyrant", "Nazi", "President Trump", "Vladimir Putin", "Trump", "Putin", "Drumph","pee-pee", "urine", "golden shower", "leaker", "leak", "cheeto", "President", "Russian", "Kremlin", "Russia", "prostitute", "grifter", "bed", "Obama", "racism"]
-verbs = ["tinkled", "flirted", "touched", "signed", "voted", "betrayed", "lied", "nutted, ""dog whistled", "peed", "urinated", "unload", "leaked", "said", "tweeted", "reported", "soaked", "grifted", "undid", "pinched", "kissed", "sucked", "laughed", "stroked", "dumped", "impeached"]
+nouns = ["dispute", "drama", "debate", "nominee", "nomination", "Senator", "unnamed Source", "dictator", "Hitler", "fascist", "tyrant", "Nazi", "President Trump", "Vladimir Putin", "Trump", "Putin", "Drumph","pee-pee", "urine", "golden shower", "leaker", "leak", "cheeto", "President", "Russian", "Kremlin", "Russia", "prostitute", "grifter", "bed", "Obama", "racism"]
+verbs = ["explained", "tinkled", "flirted", "touched", "signed", "voted", "betrayed", "lied", "nutted, ""dog whistled", "peed", "urinated", "unload", "leaked", "said", "tweeted", "reported", "soaked", "grifted", "undid", "pinched", "kissed", "sucked", "laughed", "stroked", "dumped", "impeached"]
 prepPhrases = ["on top of", "behind", "below", "underneath", "around", "between", "next to", "beneath", "betwixt"]
 adverbs = ["when", "while", "slyly", "trecherously", "secretly", "subsequently"]
 adjectives = ["racist", "golden", "fluid", "orange", "cheeto-colored", "stupid", "xenophobic", "divisive", "misogynistic", "hateful", "dishonest", "greedy"]
@@ -17,28 +17,28 @@ def chooseRandomWord(wordSet):
 
 
 def composeAdverbPrep():
-    return chooseRandomWord(adverbs).capitalize() + " " + chooseRandomWord(nouns) + " " + chooseRandomWord(verbs) + " " + chooseRandomWord(prepPhrases) + " " + chooseRandomWord(adjectives) + " " + chooseRandomWord(nouns)
+    return chooseRandomWord(adverbs).capitalize() + " " + chooseRandomWord(nouns).capitalize() + " " + chooseRandomWord(verbs).capitalize() + " " + chooseRandomWord(prepPhrases) + " " + chooseRandomWord(adjectives).capitalize() + " " + chooseRandomWord(nouns).capitalize()
 
 def composeSimple():
-    return chooseRandomWord(nouns).capitalize() + " " + chooseRandomWord(verbs)
+    return chooseRandomWord(nouns).capitalize() + " " + chooseRandomWord(verbs).capitalize()
 
 def compoundSimple():
-    return chooseRandomWord(nouns).capitalize() + " " + chooseRandomWord(verbs) + " " + chooseRandomWord(conjunctions) + " "+ chooseRandomWord(nouns) + " " + chooseRandomWord(verbs)
+    return chooseRandomWord(nouns).capitalize() + " " + chooseRandomWord(verbs).capitalize() + ", " + chooseRandomWord(conjunctions).capitalize() + " "+ chooseRandomWord(nouns).capitalize() + " " + chooseRandomWord(verbs).capitalize()
 
 
 adjList = []
 
 for i in range(random.randint(0,3)):
-    adjList.append(chooseRandomWord(adjectives))
+    adjList.append(chooseRandomWord(adjectives).capitalize())
 
 
 def composeHeadline():
-    return "Why " + chooseRandomWord(nouns) + " " + chooseRandomWord(verbs) + " " + chooseRandomWord(prepPhrases) + " " + " ".join(adjList) + " " + chooseRandomWord(nouns)
+    return "Why " + chooseRandomWord(nouns).capitalize() + " " + chooseRandomWord(verbs).capitalize() + " " + chooseRandomWord(prepPhrases) + " " + " ".join(adjList) + " " + chooseRandomWord(nouns).capitalize()
 
 
 
 
-functionArray = [composeSimple, composeAdverbPrep, compoundSimple, composeHeadline]
+functionArray = [composeSimple, composeAdverbPrep, compoundSimple, composeHeadline, composeAdverbPrep, composeHeadline, compoundSimple]
 outfile = open("headlines.txt", "w+")
 for i in range(10):
     outfile.write(functionArray[random.randint(0,len(functionArray)-1)]() + "\n") 

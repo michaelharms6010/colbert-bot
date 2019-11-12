c298f80301ce96a66c19ead7df66040dc6e7995b
Michael Harms <michael.harms6010@gmail.com>
2019-11-12 08:47:23 -0600

    becoming self-aware

--- peepeebot.py
+++ peepeebot.py
@@ -1,16 +1,17 @@
 import random
+## Todos:
+# Add hearsay format
+# 
 
-# Add hearsay
-
-nouns = ["dispute", "drama", "debate", "nominee", "nomination", "Senator", "unnamed Source", "dictator", "Hitler", "fascist", "tyrant", "Nazi", "President Trump", "Vladimir Putin", "Trump", "Putin", "Drumph","pee-pee", "urine", "golden shower", "leaker", "leak", "cheeto", "President", "Russian", "Kremlin", "Russia", "prostitute", "grifter", "bed", "Obama", "racism"]
-verbs = ["explained", "tinkled", "flirted", "touched", "signed", "voted", "betrayed", "lied", "nutted, ""dog whistled", "peed", "urinated", "unload", "leaked", "said", "tweeted", "reported", "soaked", "grifted", "undid", "pinched", "kissed", "sucked", "laughed", "stroked", "dumped", "impeached"]
-prepPhrases = ["on top of", "behind", "below", "underneath", "around", "between", "next to", "beneath", "betwixt"]
-adverbs = ["when", "while", "slyly", "trecherously", "secretly", "subsequently"]
-adjectives = ["unhinged", "racist", "golden", "fluid", "orange", "cheeto-colored", "stupid", "xenophobic", "divisive", "misogynistic", "hateful", "dishonest", "greedy"]
+nouns = ["whistleblower", "dispute", "drama", "debate", "nominee", "nomination", "Senator", "unnamed Source", "dictator", "Hitler", "fascist", "tyrant", "Nazi", "President Trump", "Vladimir Putin", "Trump", "Putin", "Drumph","pee-pee", "urine", "golden shower", "leaker", "leak", "cheeto", "President", "Russian", "Kremlin", "Russia", "prostitute", "grifter", "bed", "Obama", "racism"]
+verbs = ["raged", "blew", "explained", "tinkled", "flirted", "touched", "signed", "voted", "betrayed", "lied", "nutted, ""dog whistled", "peed", "urinated", "unload", "leaked", "said", "tweeted", "reported", "soaked", "grifted", "undid", "pinched", "kissed", "sucked", "laughed", "stroked", "dumped", "impeached"]
+prepPhrases = ["within", "among", "on top of", "behind", "below", "underneath", "around", "between", "next to", "beneath", "betwixt"]
+adverbs = ["impulsively", "racistly", "when", "while", "slyly", "trecherously", "secretly", "subsequently"]
+adjectives = ["impulsive", "unhinged", "racist", "golden", "fluid", "orange", "cheeto-colored", "stupid", "xenophobic", "divisive", "misogynistic", "hateful", "dishonest", "greedy"]
 
 conjunctions = [ "and", "or", "but", "for" , "yet" ]
 questionWords= ["what", "why", "when", "how"]
-sources = ["Unnamed Source", "Anonymous Source", "Senator", "Congressman", "Congresswoman", "Veteran Journalist", "Veteran", "Intelligence Official"]
+sources = ["whistleblower", "Unnamed Source", "Anonymous Source", "Senator", "Congressman", "Congresswoman", "Veteran Journalist", "Veteran", "Intelligence Official"]
 
 # adverb noun verb prepphrase adjective noun
 
@@ -42,12 +43,9 @@ def composeQuestion():
     return "Is " + chooseRandomWord(nouns).capitalize() + " " + chooseRandomWord(prepPhrases) + " a " + chooseRandomWord(adjectives).capitalize() + " " + chooseRandomWord(nouns).capitalize() + "?"
 
 
-functionArray = [composeSimple, composeAdverbPrep, compoundSimple, composeHeadline, composeAdverbPrep, composeHeadline, compoundSimple]
+functionArray = [composeQuestion, composeSimple, composeAdverbPrep, compoundSimple, composeHeadline, composeAdverbPrep, composeHeadline, compoundSimple]
 outfile = open("headlines.txt", "w+")
 for i in range(10):
     outfile.write(functionArray[random.randint(0,len(functionArray)-1)]() + "\n") 
-
-print composeQuestion()
-
 
 
